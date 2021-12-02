c6cfdbf369a885f7ff035646335c8fb1a136fb6f
rmt31 <rahmathidayat31@students.amikom.ac.id>
2021-12-02 19:57:55 +0700

    Tugas kedelapan

---
+++ Hasil.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TugasDelegateEvent
+{
+    
+    public partial class Hasil : Form
+    {
+        
+      
+
+        public Hasil()
+        {
+            
+            InitializeComponent();
+        }
+        private void Tulis(int operasi, int a, int b, int hasil)
+        {
+            char[] operasi_simbol = { '+', '-', '*', '/' };
+            string[] operasi_str = { "Penambahan", "Pengurangan", "Perkalian", "Pembagian" };
+            OutputHasil.Items.Add($"Hasil {(operasi_str[operasi]).ToLower()} {a} {operasi_simbol[operasi]} {b} = {hasil}" );
+        }
+        private void Menghitung(object sender, EventArgs e)
+        {
+            Kalkulator hitung = new Kalkulator();
+            hitung.penghubung += Tulis;
+            hitung.ShowDialog();
+        }
+    }
+}
---
+++ Kalkulator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TugasDelegateEvent
+{
+    public partial class Kalkulator : Form
+    {
+        public delegate void Penghubung(int operasi, int a, int b, int hasil);
+        public event Penghubung penghubung;
+        public Kalkulator()
+        {
+            InitializeComponent();
+        }
+        
+        private void Click_Proses(object sender, EventArgs e)
+        {
+            string operasi = OperasiPilihan.SelectedItem.ToString();
+            int a = Convert.ToInt32(NilaiA.Text);
+            int b = Convert.ToInt32(NilaiB.Text);
+            int hasil=0;
+            switch (OperasiPilihan.SelectedIndex)
+            {
+                case 0:
+                    hasil = a + b;
+                    break;
+                case 1:
+                    hasil = a - b;
+                    break;
+                case 2:
+                    hasil = a * b;
+                    break;
+                case 3:
+                    hasil = a / b;
+                    break;
+                default:
+                    hasil = 0;
+                    break;
+            }
+            this.penghubung(OperasiPilihan.SelectedIndex, a, b, hasil);
+        }
+    }
+}
